024a294f7ed0e8768e4852f5128d85445f80eb95
rtamura30 <rtamura30@gmail.com>
2025-04-27 05:19:23 +0000

    feat(ch2): if文の型チェック

--- src/arith.test.ts
+++ src/arith.test.ts
@@ -1,5 +1,5 @@
 import { expect } from "@std/expect";
-import { test } from "@std/testing/bdd";
+import { describe, test } from "@std/testing/bdd";
 import { parseArith } from "tiny-ts-parser";
 import { Type, typecheck } from "./arith.ts";
 
@@ -20,5 +20,16 @@ test("trueはBoolean型と判定される", () => ok("true", "Boolean"));
 test("falseはBoolean型と判定される", () => ok("false", "Boolean"));
 test("Number型はNumber型と判定される", () => ok("42", "Number"));
 
-test("Number型とNumber型のaddはNumber型と判定される", () => ok("42 + 42", "Number"));
-test("Number型とBoolean型のaddは例外を発生させる", () => ng("42 + true", /number expected/));
+describe("add", () => {
+    test("Number型とNumber型のaddはNumber型と判定される", () => ok("42 + 42", "Number"));
+    test("Number型とBoolean型のaddは型エラーが発生する", () => ng("42 + true", /number expected/));
+});
+
+describe("if", () => {
+    test("if文の条件がBooleanでないとき、型エラーが発生する", () =>
+        ng("if (42) { 42 } else { 42 }", /boolean expected/));
+    test("then/elseの型が同一のとき、その型がif文の型と判定される", () =>
+        ok("if (true) { return 42 } else { return 42 }", "Number"));
+    test("then/elseの型が異なる場合は例外を発生させる", () =>
+        ng("if (true) { return 42 } else { return true }", /branches must have the same type/));
+});
